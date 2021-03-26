a00dc20340ed242e4f7d78a379c6827a96aa5184
Eduardo <xeduardox54@hotmail.com>
2021-03-25 20:33:38 -0500

    Punto 5

--- src/content.js
+++ src/content.js
@@ -5,9 +5,9 @@ function Part(props) {
 function Content(props) {
     return(
         <div>
-            <Part part={props.part1} exercises={props.exercises1}/>
-            <Part part={props.part2} exercises={props.exercises2}/>
-            <Part part={props.part3} exercises={props.exercises3}/>
+            <Part part={props.part1.name} exercises={props.part1.exercises}/>
+            <Part part={props.part2.name} exercises={props.part2.exercises}/>
+            <Part part={props.part3.name} exercises={props.part3.exercises}/>
         </div>
     );
 }
